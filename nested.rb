3922034d2196dccc1d2786359246fd564a83a5a3
Nora Vogt <noraevogt@gmail.com>
2018-11-25 14:55:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-programmer_hash[:grace_hopper]
+		 programmer_hash[:grace_hopper]
 
 end
 
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
 
-programmer_hash[:alan_kay][:known_for]
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages][0]
+		 programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -86,11 +86,11 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {
+		 programmer_hash[:yukihiro_matsumoto] = {
 																					:known_for => "Ruby",
 																					:languages => ["LISP", "C"]
 																				}
-programmer_hash
+																				programmer_hash
 end
 
 def changing_alan
@@ -111,6 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 
 end
