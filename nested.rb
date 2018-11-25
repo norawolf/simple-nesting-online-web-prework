8b1ef3ca286e2d63f12b4944dacd15668d9862fb
Nora Vogt <noraevogt@gmail.com>
2018-11-25 14:54:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,6 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -87,7 +86,10 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihero_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash[:yukihiro_matsumoto] = {
+																					:known_for => "Ruby", 
+																					:languages => ["LISP", "C"]
+																				}
 programmer_hash
 end
 
